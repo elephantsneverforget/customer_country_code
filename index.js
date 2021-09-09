935f4ab70af06bcf2b304f58dcf5cfb59eccef75
Jon <joncairo@gmail.com>
2021-09-09 07:32:26 -0600

    Update notes.

--- index.js
+++ index.js
@@ -7,9 +7,23 @@ Insert this code prior to the GTM Suite data layer
 event you want merge data with.
 
 For ex. 
- ElevarGtmSuite.utils.pushToDataLayer({... // Our push
+ElevarGtmSuite.utils.pushToDataLayer({... // Our push
 window.ElevarGtmSuite.handlers.checkoutComplete({ // Event you want to effect
+
+Depending on context you may want to use the following script presence check
+window.addEventListener('load', () => {
+  if(window.ElevarGtmSuite){
+    // Custom code here...
+    // window.ElevarGtmSuite.utils.pushToDataLayer({event: "hello"})
+  }
+});
+
+The example below is tied into the order processing notes
+and uses a preexisting script presence check.
 */
+
+
+
 if (config.event_config.checkout_complete && Shopify.Checkout && Shopify.Checkout.page === "thank_you") {
     ElevarGtmSuite.utils.pushToDataLayer({
         "event": "add_user_props",
@@ -18,5 +32,5 @@ if (config.event_config.checkout_complete && Shopify.Checkout && Shopify.Checkou
                 "customer_country_code": {{- customer.default_address.country | json -}},
             }
         }
-	});
+    });
 }
