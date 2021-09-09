d8867c79e8d6db7cfd7b345df022722e0c9c1c61
Jon <joncairo@gmail.com>
2021-09-09 06:57:33 -0600

    Update notes

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 /*
 Adds a add_user_props event to the DL
 Contents of the event are merged with subsequent
-Elevar DL events 
+Elevar DL events
+
+Insert this code prior to the GTM Suite data layer
+event you want merge data with.
+
+For ex. 
+ ElevarGtmSuite.utils.pushToDataLayer({... // Our push
+window.ElevarGtmSuite.handlers.checkoutComplete({ // Event you want to effect
 */
 if (config.event_config.checkout_complete && Shopify.Checkout && Shopify.Checkout.page === "thank_you") {
     ElevarGtmSuite.utils.pushToDataLayer({
